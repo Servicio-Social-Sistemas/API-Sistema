ccd9f6da089bc30baf6c209d7fcfa09354a0406f
Texhnolyze47 <ichigobankai136@gmail.com>
2023-07-12 17:30:34 -0600

    add: Se aumenta el contador de numero de pregunta

--- src/main/java/com/texhnolyze/formulariogeo/servicio/EncuestaServicio.java
+++ src/main/java/com/texhnolyze/formulariogeo/servicio/EncuestaServicio.java
@@ -1,8 +1,12 @@
 package com.texhnolyze.formulariogeo.servicio;
 
 import com.texhnolyze.formulariogeo.modelo.Encuesta;
+import com.texhnolyze.formulariogeo.modelo.PosicionGeografica;
+import com.texhnolyze.formulariogeo.modelo.Pregunta;
 import com.texhnolyze.formulariogeo.repositorio.EncuestaRespositorio;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EncuestaServicio {
@@ -14,6 +18,18 @@ public class EncuestaServicio {
     }
 
     public void save(Encuesta encuesta){
+        // Verificar si se alcanzó el límite de 8 preguntas
+        if (encuesta.getPreguntas().size() >= 8) {
+            throw new IllegalArgumentException("Se ha alcanzado el límite máximo de preguntas (8) en la encuesta.");
+        }
+
+        // Asignar el número de pregunta a cada pregunta en la lista
+        List<Pregunta> preguntas = encuesta.getPreguntas();
+        for (int i = 0; i < preguntas.size(); i++) {
+            Pregunta pregunta = preguntas.get(i);
+            pregunta.setNumeroPregunta(i + 1);
+        }
+
         encuestaRespositorio.save(encuesta);
     }
 
