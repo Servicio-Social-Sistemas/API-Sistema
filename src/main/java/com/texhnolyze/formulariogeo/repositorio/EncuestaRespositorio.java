b7aabb576970ae6fd192f5d6559accd00c123955
Texhnolyze47 <ichigobankai136@gmail.com>
2023-07-12 17:28:34 -0600

    typo: se agrego la anotacion de Repository

--- src/main/java/com/texhnolyze/formulariogeo/repositorio/EncuestaRespositorio.java
+++ src/main/java/com/texhnolyze/formulariogeo/repositorio/EncuestaRespositorio.java
@@ -2,7 +2,9 @@ package com.texhnolyze.formulariogeo.repositorio;
 
 import com.texhnolyze.formulariogeo.modelo.Encuesta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EncuestaRespositorio extends JpaRepository<Encuesta, Long>{
 
 }
