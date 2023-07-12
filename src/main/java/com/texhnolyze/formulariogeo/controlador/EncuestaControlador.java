af54d117d62edf44db7aca48832b3ff30ebf272c
Texhnolyze47 <ichigobankai136@gmail.com>
2023-07-12 13:27:45 -0600

    add: EncuestaControlador

---
+++ src/main/java/com/texhnolyze/formulariogeo/controlador/EncuestaControlador.java
@@ -0,0 +1,28 @@
+package com.texhnolyze.formulariogeo.controlador;
+
+import com.texhnolyze.formulariogeo.dto.EncuestaDto;
+import com.texhnolyze.formulariogeo.modelo.Encuesta;
+import com.texhnolyze.formulariogeo.servicio.EncuestaServicio;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/encuesta")
+public class EncuestaControlador {
+
+    private final EncuestaServicio encuestaServicio;
+
+    public EncuestaControlador(EncuestaServicio encuestaServicio) {
+        this.encuestaServicio = encuestaServicio;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addEncuesta(@RequestBody Encuesta encuesta){
+        encuestaServicio.save(encuesta);
+        return ResponseEntity.ok("Encuesta agregada");
+    }
+
+}
